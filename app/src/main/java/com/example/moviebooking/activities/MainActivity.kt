944a1cab398de085ceaa0b7c721486f9bc6e947a
YagnikBavishi <yagnikbavishi003@gmail.com>
2022-03-29 09:41:07 +0530

    Completed UI For Login Screen

---
+++ app/src/main/java/com/example/moviebooking/activities/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.moviebooking.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.moviebooking.constants.Constants
+import com.example.moviebooking.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            delay(Constants.DELAY_TIME)
+            val intent = Intent(this@MainActivity, LogInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
